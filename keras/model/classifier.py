969a221c58ae179076952066e1c19b06466be783
jwyeeh-dev <jwyeeh@gmail.com>
2022-05-22 11:08:50 +0900

    git model add

--- keras/model/classifier.py
+++ keras/model/classifier.py
@@ -1,4 +1,5 @@
 import sys
+from jupyterlab_server import LabConfig
 import numpy as np
 import tensorflow as tf
 
@@ -13,11 +14,33 @@ class Classifier(object):
         # need a adjustment
         pass
 
-    def model(self):
-        model = Sequential()
-        
-        
-        return model
+    def model_si(self):
+        model_si = Sequential()
+        model_si.add(Conv2D(filters= 512, kernel_size= (1,3), strides= 1, padding = 0))
+        model_si.add(BatchNormalization(momentum= 0.8))
+        model_si.add(LeakyReLU(alpha= 0.2))
+        model_si.add(Conv2D(filters= 256, kernel_size= (1,1), strides= 1, padding= 0))
+        model_si.add(BatchNormalization(momentum= 0.8))
+        model_si.add(LeakyReLU(alpha= 0.2))        
+        model_si.add(Conv2D(filters= 256, kernel_size= (1,1), strides= 1, padding= 0))
+        model_si.add(BatchNormalization(momentum= 0.8))
+        model_si.add(LeakyReLU(alpha= 0.2))        
+        model_si.add(Conv2D(filters= 2, kernel_size= (1,1), strides= 1, padding= 0))
+
+        return model_si
+
+    def model_cell(self):
+        model_cell = Sequential()
+        model_cell.add(Conv2D(filters= 64, kernel_size= (1,3), strides= 1, padding= 0))
+        model_cell.add(BatchNormalization(momentum=0.8))
+        model_cell.add(LeakyReLU(alpha= 0.3))
+        model_cell.add(Conv2D(filters= 64, kernel_size= (1,1), strides = 1, padding= 0))
+        model_cell.add(BatchNormalization(momentum=0.8))
+        model_cell.add(LeakyReLU(alpha= 0.2))
+
+
+
+        return model_cell
 
     def summary(self):
         return self.Generator.summary()
